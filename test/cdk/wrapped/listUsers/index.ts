9e7f28c6a3bcd4b3cc72860f6b0cf21ce97eef1f
Drew Heiss <drewdheiss@gmail.com>
2025-06-17 11:26:35 -0400

    remove test cdk output

--- test/cdk/wrapped/listUsers/index.ts
+++
@@ -1,9 +0,0 @@
-// Auto-generated wrapper
-import handler from "./handler";
-import { wrapHandler } from "../../helpers/wrapHandler";
-
-export const main = wrapHandler(handler, {
-  auth: function auth(event) {
-    return { user: "drew" };
-  },
-});
